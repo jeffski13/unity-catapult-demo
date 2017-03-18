f24bcaa0c57d6d32aa0a0f6d149f279b945c2103
jeffski13 <jeffski@okstate.edu>
2017-03-18 00:36:30 -0500

    initial commit of catapult project

---
+++ NexusClass/Assets/Scripts/SphereBehavior.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereBehavior : MonoBehaviour {
+    //true if on last frame, fire button was down
+    private bool fire1Down = false;
+    //number of frames fire button was held
+    private int framesHeld = 0;
+    //was missle fired?
+    private bool mizzilesFired = false;
+
+    [SerializeField] private CrashCam cam;
+    
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Space)) {
+            //make sure missile has not been launched 
+            if (!mizzilesFired) {
+                framesHeld++;
+                if(framesHeld%50 == 0) {
+                    cam.AddNextPowerBlock();
+                }
+            }
+
+        }
+        else {
+            if(framesHeld > 0 && !mizzilesFired) {
+                //FIRE ZE MISSILES!
+                mizzilesFired = true;
+                FireUpAndOver(framesHeld);
+            }
+        }
+
+    }
+
+    private void FireUpAndOver() {
+        GetComponent<Rigidbody>().AddForce((transform.up + transform.forward) * 200);
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    private void FireUpAndOver(int power) {
+        GetComponent<Rigidbody>().AddForce((transform.up + transform.forward) * power);
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    
+}
